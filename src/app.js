45db03d9301883bbc78b85094ba4228e8a18bc97
Rakesh <rakeshsabbani1902@gmail.com>
2024-11-01 13:48:50 +0530

    Playing with Routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const app = express();
 
+//It will work for abc and ac , b is optional
+app.get("/ab?c",(req,res)=>{
+    res.send({firstname : "ab", lastname :"ab"})
+})
+
+//it will match the pattern 
+app.get("/ab+c",(req,res)=>{
+    res.send({firstname : "MS", lastname :"Dhoni"})
+})
+
+//Anything between ab and cd works 
+app.get("/ab*cd",(req,res)=>{
+    res.send({firstname : "Virat", lastname :"Kohli"})
+})
+
+//bc is optional
+app.get("/a(bc)?d",(req,res)=>{
+    res.send({firstname : "Virat", lastname :"Kohli"})
+})
+
+app.get(/a/,(req,res)=>{
+    res.send("Regx Expression")
+})
 
 
+app.get(/.*fly$/,(req,res)=>{
+    res.send("Regx Expression")
+})
+
+app.get("/user",(req,res)=>{
+    console.log(req.query);
+    res.send({firstname : "Rakesh", lastname :"Sabbani"})
+})
+
+
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname : "Rakesh", lastname :"Sabbani"})
+})
 
 //this will only GET Call to /user
 app.get("/user",(req,res)=>{
