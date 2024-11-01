cd1372ccb270402cc3c664ca91f80ad7f8c50243
Rakesh <rakeshsabbani1902@gmail.com>
2024-11-01 13:01:25 +0530

    Exploring Get, Post, Delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use("",(req,res)=>{
-    res.send("Namaste  dashboard");
-});
 
-app.use("/profile",(req,res)=>{
-    res.send("profile");
-});
 
-app.use("/test",(req,res)=>{
-    res.send("Hello This is rakesh")
+
+//this will only GET Call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname : "Rakesh", lastname :"Sabbani"})
 })
+
+
+//this will only POST Call to /user
+app.post("/user",(req,res)=>{
+   //Save data to DB
+   res.send("Data saved successfully to the database");
+})
+
+//this will only DELETE Call to /user
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
+
+
+
+//this will call all the HTTP Method API calls to /test
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server")
+})
+
+
 
 app.listen(7777,()=>{
     console.log("server is running successfully on 7777")
