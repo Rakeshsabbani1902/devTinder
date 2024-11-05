1c43b4ce931535a51d2c431dff6418badc5be9a3
Rakesh <rakeshsabbani1902@gmail.com>
2024-11-05 23:09:45 +0530

    Created login API

--- src/app.js
+++ src/app.js
@@ -1,28 +1,69 @@
 const express = require('express');
 const connectDB = require('./config/database')
 const User = require("./models/user")
+const {validateSignUpData}= require("./utils/validation")
+const bcrypt = require("bcrypt");
+const validator = require("validator")
 const app = express();
 
 
 //express gives  a middleware whoch converts JSON objects to js object which sever can understand 
 app.use(express.json());
-app.post("/signup",async (req,res)=>{
-   
-    console.log(req.body);
+app.post("/signup", async (req,res)=>{
 
-    const userObj = req.body;
+
+    try{
+    //validation of data 
+    validateSignUpData(req);
+
+
+    //Encrypt the password
+    const {firstName,lastName,emailId,password}= req.body;
+
+    const passwordHash=  await bcrypt.hash(password,10);
+    console.log(passwordHash)
+
+    const userObj = {
+        firstName,lastName ,emailId,
+        password : passwordHash,
+    };
     //creating a new instance of user model and passing data into it
     const user = new User(userObj);
     //save the data into database 
 
-    try{
+   
         await user.save();
     res.send("User added successfully ")}
     catch(err){
-        res.status(400).send("Error saving the User" + err.message);
+        res.status(400).send("ERROR :" + err.message);
     }
     
 })
+
+app.post("/login", async (req,res)=>{
+  try{
+    const {emailId,password}= req.body;
+    // if(!validator.isEmail(emailId)){
+    //     throw new Error("Email is not valid")
+    //  };
+
+     const user = await User.findOne({emailId:emailId});
+     if(!user){
+        throw new Error("Invalid Credentials")
+     }
+     const isPasswordValid = await bcrypt.compare(password,user.password)
+     if(isPasswordValid){
+        res.send("Login Successfull !!!")
+     }
+     else{
+        throw new Error("Invalid Credentials  ")
+     }
+  }
+  catch(err){
+    res.status(400).send("ERROR :" + err.message);
+  }
+})
+
 
 //get user by email 
 app.get("/user", async (req,res)=>{
@@ -158,4 +199,4 @@ connectDB().then(()=>{
  })
  
 
-
+//NEVER TRUST REQ.BODY
